86217694ede31c3a257d267be761da64b3053465
FiveMoutainTomoto <2257485855@qq.com>
2025-01-01 22:07:50 +0800

    (+)function.py, func extract_page_range

---
+++ pdf-handle-script/function.py
@@ -0,0 +1,20 @@
+import fitz
+import os
+
+
+# 提取文件中的页码范围到新的pdf
+def extract_page_range(file_path, start_page, end_page):
+    doc = fitz.open(file_path)
+    output_doc = fitz.open()
+    print(f'文件 "{os.path.basename(file_path)}" 打开成功！')
+
+    for page_num in range(start_page - 1, end_page):
+        output_doc.insert_pdf(doc, from_page=page_num, to_page=page_num)
+    print(f'提取页码 {start_page} 到 {end_page} 页完成！')
+
+    output_path = file_path.replace(".pdf", f"_extracted_{start_page}to{end_page}.pdf")
+    output_doc.save(output_path)
+    print(f'已保存到 "{output_path}" ')
+
+    output_doc.close()
+    doc.close()
