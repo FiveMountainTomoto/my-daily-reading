8936f7b9c86982130073c2ea710fed3259095f96
FiveMoutainTomoto <2257485855@qq.com>
2025-01-01 22:09:46 +0800

    Day 1, text 1

---
+++ pdf-handle-script/main.py
@@ -0,0 +1,6 @@
+from function import *
+
+file_path = r'E:\Data\英文外刊\awesome-english-ebooks\01_economist\te_2024.12.21\TheEconomist.2024.12.21.pdf'
+start_page = 97
+end_page = 98
+extract_page_range(file_path, start_page, end_page)
