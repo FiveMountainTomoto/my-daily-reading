382582ab536697c9a79ad50eb12e798f4535dada
FiveMountainTomoto <2257485855@qq.com>
2025-01-04 11:48:22 +0800

    Day4, text1

--- pdf-handle-script/main.py
+++ pdf-handle-script/main.py
@@ -1,6 +1,6 @@
 from function import *
 
-file_path = r"E:\Data\英文外刊\awesome-english-ebooks\02_new_yorker\2024.01.01\new_yorker.2024.01.01.pdf"
-start_page = 63
-end_page = 66
+file_path = r"E:\Data\英文外刊\awesome-english-ebooks\01_economist\te_2024.01.13\TheEconomist.2024.01.13.pdf"
+start_page = 9
+end_page = 12
 extract_page_range(file_path, start_page, end_page)
