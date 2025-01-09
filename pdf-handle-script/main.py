dda7337871afc81eca52cb5e10e0f9bcfd3b1dbc
FiveMountainTomoto <2257485855@qq.com>
2025-01-09 12:59:12 +0800

    Day5, text1

--- pdf-handle-script/main.py
+++ pdf-handle-script/main.py
@@ -1,6 +1,9 @@
 from function import *
 
-file_path = r"E:\Data\英文外刊\awesome-english-ebooks\01_economist\te_2024.01.13\TheEconomist.2024.01.13.pdf"
-start_page = 9
-end_page = 12
-extract_page_range(file_path, start_page, end_page)
+file_path = r"E:\Data\英文外刊\awesome-english-ebooks\02_new_yorker\2024.01.08\new_yorker.2024.01.08.pdf"
+start_page = 24
+end_page = 26
+# extract_page_range(file_path, start_page, end_page)
+
+pdf_path = r"E:\Data\英文外刊\my-daily-reading\2025\January\8th\注释版Has Gratuity Culture Reached a Tipping Point.pdf"
+export_pdf_to_jpeg(pdf_path)
