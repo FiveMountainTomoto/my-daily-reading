5695e49fa419ed9241c94faff4837322636798bd
FiveMountainTomoto <2257485855@qq.com>
2025-01-10 17:14:12 +0800

    Day6, text1

---
+++ 2025/January/10th/README.md
@@ -0,0 +1,9 @@
+# A Passion-Fruit Devotee’s Pilgrimage West
+
+**节选自《The New Yorker》JAN. 15, 2024**
+
+词数约800，较易
+
+百香果魔怔人写的文章，越读越饿。
+
+主要是描述味道那一段用的超纲词特别多，复杂的句子结构并不多，比较易读，难读的地方也有，但是不多。
--- pdf-handle-script/main.py
+++ pdf-handle-script/main.py
@@ -1,9 +1,9 @@
 from function import *
 
-file_path = r"E:\Data\英文外刊\awesome-english-ebooks\02_new_yorker\2024.01.08\new_yorker.2024.01.08.pdf"
-start_page = 24
-end_page = 26
+file_path = r"E:\Data\英文外刊\awesome-english-ebooks\02_new_yorker\2024.01.15\new_yorker.2024.01.15.pdf"
+start_page = 100
+end_page = 102
 # extract_page_range(file_path, start_page, end_page)
 
-pdf_path = r"E:\Data\英文外刊\my-daily-reading\2025\January\8th\注释版Has Gratuity Culture Reached a Tipping Point.pdf"
+pdf_path = r"E:\Data\英文外刊\my-daily-reading\2025\January\10th\注释版A Passion-Fruit Devotee’s Pilgrimage West.pdf"
 export_pdf_to_jpeg(pdf_path)
